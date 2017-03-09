1246f07c8c31e1feee5ea374eb0b90becb30184f
hsk <sakurai777@gmail.com>
2017-03-09 12:47:44 +0900

    added files to prolog5

---
+++ prolog5/equirec.pl
@@ -0,0 +1,69 @@
+:- discontiguous((\-)/2).
+:- discontiguous((/-)/2).
+:- op(1200, xfx, ['--', where]).
+:- op(1100, xfy, [in]).
+:- op(1050, xfy, ['=>']).
+:- op(920, xfx, ['==>', '==>>', '<:']).
+:- op(910, xfx, ['/-', '\\-']).
+:- op(600, xfy, ['::', '#', as]).
+:- op(500, yfx, ['$', !, tsubst, tsubst2, subst, subst2, tmsubst, tmsubst2]).
+term_expansion((A where B), (A :- B)).
+:- style_check(-singleton).
+:- use_module(rtg).
+
+x ::= atom.
+t ::= (t -> t) | rec(x, t) | x.
+m ::= x | (fn(x : t) -> m) | m $ m.
+v ::= fn(x : t) -> m.
+
+                  J![(J -> S)] tsubst S                   :- x(J).
+                  X![(J -> S)] tsubst X                   :- x(X).
+         (T1 -> T2)![(J -> S)] tsubst (T1_ -> T2_)        :- T1![(J -> S)] tsubst T1_, T2![(J -> S)] tsubst T2_.
+         rec(X, T1)![(J -> S)] tsubst rec(X, T1_)         :- T1![X, (J -> S)] tsubst2 T1_.
+               T![X, (X -> S)] tsubst2 T.
+               T![X, (J -> S)] tsubst2 T_                 :- T![(J -> S)] tsubst T_.
+
+                  J![(J -> M)] subst M                    :- x(J).
+                  X![(J -> M)] subst X                    :- x(X).
+(fn(X1 : T1) -> M2)![(J -> M)] subst (fn(X1 : T1) -> M2_) :- M2![X1, (J -> M)] subst2 M2_.
+            M1 $ M2![(J -> M)] subst (M1_ $ M2_)          :- M1![(J -> M)] subst M1_, M2![(J -> M)] subst M2_.
+               S![J, (J -> M)] subst2 S.
+               S![X, (J -> M)] subst2 M_                  :- S![(J -> M)] subst M_.
+
+getb(Γ, X, B) :- member(X - B, Γ).
+gett(Γ, X, T) :- getb(Γ, X, bVar(T)).
+
+Γ /- (fn(X) -> M12) $ V2 ==> R        where v(V2), M12![(X -> V2)] subst R.
+Γ /- V1 $ M2             ==> V1 $ M2_ where v(V1), Γ /- M2 ==> M2_.
+Γ /- M1 $ M2             ==> M1_ $ M2 where Γ /- M1 ==> M1_.
+Γ /- M                  ==>> M_       where Γ /- M ==> M1, Γ /- M1 ==>> M_.
+Γ /- M                  ==>> M.
+
+compute(Γ, rec(X, S1), T) :- S1![(X -> rec(X, S1))] tsubst T.
+simplify(Γ, T, T_)        :- compute(Γ, T, T1), simplify(Γ, T1, T_).
+simplify(Γ, T, T).
+
+Γ          /-          S = T          :- ([] ; Γ) \- S = T.
+(Seen ; Γ) \-          S = T          :- member((S, T), Seen).
+(Seen ; Γ) \-          X = Y          :- x(X), x(Y).
+(Seen ; Γ) \- (S1 -> S2) = (T1 -> T2) :- (Seen ; Γ) \- S1 = T1, (Seen ; Γ) \- S2 = T2.
+(Seen ; Γ) \- rec(X, S1) = T          :- S = rec(X, S1), S1![(X -> S)] tsubst S1_, ([(S, T) | Seen] ; Γ) \- S1_ = T.
+(Seen ; Γ) \-          S = rec(X, T1) :- T = rec(X, T1), T1![(X -> T)] tsubst T1_, ([(S, T) | Seen] ; Γ) \- S = T1_.
+
+Γ /-                  X : T           where x(X), gett(Γ, X, T).
+Γ /- (fn(X : T1) -> M2) : (T1 -> T2_) where [X - bVar(T1) | Γ] /- M2 : T2_.
+Γ /-            M1 $ M2 : T12         where Γ /- M1 : T1, Γ /- M2 : T2, simplify(Γ, T1, (T11 -> T12)), Γ /- T2 = T11.
+
+show_bind(Γ, bName, '').
+show_bind(Γ, bVar(T), R) :- swritef(R, ' : %w', [T]).
+show_bind(Γ, bTVar, '').
+run(eval(M), Γ, Γ) :- !, m(M), !, Γ /- M : T, !, Γ /- M ==>> M_, !, writeln(M_ : T).
+run(bind(X, Bind), Γ, [X - Bind | Γ]) :- show_bind(Γ, Bind, S), write(X), writeln(S).
+run(Ls) :- foldl(run, Ls, [], _).
+
+:- run([eval((fn(x : 'A') -> x))]).
+:- run([eval((fn(f : rec('X', ('A' -> 'A'))) -> (fn(x : 'A') -> f $ x)))]).
+:- run([eval((fn(x : 'T') -> x))]).
+:- run([bind('T', bTVar), bind(i, bVar('T')), eval(i)]).
+:- halt.
+
---
+++ prolog5/fulluntyped.pl
@@ -0,0 +1,130 @@
+:- discontiguous((\-)/2).
+:- discontiguous((/-)/2).
+:- op(1200, xfx, ['--', where]).
+:- op(1100, xfy, [in]).
+:- op(1050, xfy, ['=>']).
+:- op(920, xfx, ['==>', '==>>', '<:']).
+:- op(910, xfx, ['/-', '\\-']).
+:- op(600, xfy, ['::', '#', as]).
+:- op(500, yfx, ['$', !, tsubst, tsubst2, subst, subst2, tmsubst, tmsubst2]).
+term_expansion((A where B), (A :- B)).
+:- style_check(- singleton).
+
+w(W) :- member(W, [true, false]).                    % キーワード:
+x(X) :- \+ w(X), atom(X).                            % 識別子:
+l(L) :- atom(L) ; integer(L).                        % ラベル:
+m(M) :-                                              % 項:
+    M = true                                         % 真
+  ; M = false                                        % 偽
+  ; M = if(M1, M2, M3), m(M1), m(M2), m(M3)          % 条件式
+  ; M = 0                                            % ゼロ
+  ; M = succ(M1), m(M1)                              % 後者値
+  ; M = pred(M1), m(M1)                              % 前者値
+  ; M = iszero(M1), m(M1)                            % ゼロ判定
+  ; M = F, float(F)                                  % 浮動小数点数
+  ; M = M1 * M2, m(M1), m(M2)                        % 乗算
+  ; M = X, string(X)                                 % 文字列
+  ; M = X, x(X)                                      % 識別子
+  ; M = (fn(X) -> M1), m(M1)                         % ラムダ抽象
+  ; M = M1 $ M2, m(M1), m(M2)                        % 関数適用
+  ; M = (let(X) = M1 in M2), x(X), m(M1), m(M2)      % let式
+  ; M = {Mf}, maplist([X = M1] >> (l(X), m(M1)), Mf) % レコード
+  ; M = M1 # L, m(M1), l(L)                          % 射影
+  .
+n(N) :-                                              % 数値:
+    N = 0                                            % ゼロ
+  ; N = succ(N1), n(N1)                              % 後者値
+  .
+v(V) :-                                              % 値:
+    V = true                                         % 定数真
+  ; V = false                                        % 定数偽
+  ; V = N, n(N)                                      % 数値
+  ; V = F, float(F)                                  % 浮動小数点数
+  ; V = X, string(X)                                 % 文字列
+  ; V = (fn(X) -> M1)                                % ラムダ抽象
+  ; V = {Mf}, maplist([L=V1]>>(l(L), v(V1)), Mf)     % レコード
+  .
+
+% 置換
+
+               true![J -> M] subst true.
+              false![J -> M] subst false.
+     if(M1, M2, M3)![J -> M] subst if(M1_, M2_, M3_)     :- M1![J -> M] subst M1_, M2![J -> M] subst M2_,
+                                                            M3![J -> M] subst M3_.
+                  0![J -> M] subst 0.
+           succ(M1)![J -> M] subst succ(M1_)             :- M1![J -> M] subst M1_.
+           pred(M1)![J -> M] subst pred(M1_)             :- M1![J -> M] subst M1_.
+         iszero(M1)![J -> M] subst iszero(M1_)           :- M1![J -> M] subst M1_.
+                 F1![J -> M] subst F1                    :- float(F1).
+            M1 * M2![J -> M] subst M1_ * M2_             :- M1![J -> M] subst M1_, M2![J -> M] subst M2_.
+                 M1![J -> M] subst M1_                   :- string(M1), M1![J -> M] subst M1_.
+                  J![J -> M] subst M                     :- x(J).
+                  X![J -> M] subst X                     :- x(X).
+      (fn(X) -> M2)![J -> M] subst (fn(X) -> M2_)        :- M2![X, (J -> M)] subst2 M2_.
+            M1 $ M2![J -> M] subst (M1_ $ M2_)           :- M1![J -> M] subst M1_, M2![J -> M] subst M2_.
+(let(X) = M1 in M2)![J -> M] subst (let(X) = M1_ in M2_) :- M1![J -> M] subst M1_, M2![X, (J -> M)] subst2 M2_.
+               {Mf}![J -> M] subst {Mf_}                 :- maplist([L=Mi, L=Mi_]>>(Mi![J -> M] subst Mi_), Mf, Mf_).
+           (M1 # L)![J -> M] subst (M1_ # L)             :- M1![J -> M] subst M1_.
+                  S![J -> M] subst _                     :- writeln(error : subst(J, M, S)), fail.
+             S![J, (J -> M)] subst2 S.
+             S![X, (J -> M)] subst2 M_                   :- S![J -> M] subst M_.
+
+getb(Γ, X, B) :- member(X - B, Γ).
+
+% 評価
+
+e([L = M | Mf], M, [L = M_ | Mf], M_) :- \+ v(M).
+e([L = M | Mf], M1, [L = M | Mf_], M_) :- v(M), e(Mf, M1, Mf_, M_).
+
+Γ /- if(true, M2, _)     ==> M2.
+Γ /- if(false, _, M3)    ==> M3.
+Γ /- if(M1, M2, M3)      ==> if(M1_, M2, M3)      where Γ /- M1                   ==> M1_.
+Γ /- succ(M1)            ==> succ(M1_)            where Γ /- M1                   ==> M1_.
+Γ /- pred(0)             ==> 0.
+Γ /- pred(succ(N1))      ==> N1                   where n(N1).
+Γ /- pred(M1)            ==> pred(M1_)            where Γ /- M1                   ==> M1_.
+Γ /- iszero(0)           ==> true.
+Γ /- iszero(succ(N1))    ==> false                where n(N1).
+Γ /- iszero(M1)          ==> iszero(M1_)          where Γ /- M1                   ==> M1_.
+Γ /- F1 * F2             ==> F3                   where float(F1), float(F2), F3 is F1 * F2.
+Γ /- V1 * M2             ==> V1 * M2_             where v(V1), Γ /- M2            ==> M2_.
+Γ /- M1 * M2             ==> M1_ * M2             where Γ /- M1                   ==> M1_.
+Γ /- X                   ==> M                    where x(X), getb(Γ, X, bMAbb(M)).
+Γ /- (fn(X) -> M12) $ V2 ==> R                    where v(V2), M12![(X -> V2)]  subst R.
+Γ /- V1 $ M2             ==> V1 $ M2_             where v(V1), Γ /- M2            ==> M2_.
+Γ /- M1 $ M2             ==> M1_ $ M2             where Γ /- M1                   ==> M1_.
+Γ /- (let(X) = V1 in M2) ==> M2_                  where v(V1), M2![(X -> V1)]   subst M2_.
+Γ /- (let(X) = M1 in M2) ==> (let(X) = M1_ in M2) where Γ /- M1                   ==> M1_.
+Γ /- {Mf}                ==> {Mf_}                where e(Mf, M, Mf_, M_), Γ /- M ==> M_.
+Γ /- {Mf} # L            ==> M                    where member(L = M, Mf).
+Γ /- M1 # L              ==> M1_ # L              where Γ /- M1                   ==> M1_.
+Γ /- M                  ==>> M_                   where Γ /- M                    ==> M1, Γ /- M1 ==>> M_.
+Γ /- M                  ==>> M.
+
+evalbinding(Γ, bMAbb(M), bMAbb(M_)) :- Γ /- M ==>> M_.
+evalbinding(Γ, Bind, Bind).
+
+show_bind(Γ, bName, '').
+show_bind(Γ, bMAbb(M), R) :- swritef(R, ' = %w', [M]).
+
+run(eval(M), Γ, Γ)                     :- !, m(M), !, Γ /- M ==>> M_, !, writeln(M_), !.
+run(bind(X, Bind), Γ, [X - Bind_ | Γ]) :- evalbinding(Γ, Bind, Bind_), show_bind(Γ, Bind, S), write(X), writeln(S).
+run(Ls) :- foldl(run, Ls, [], _).
+
+:- run([eval(true)]).
+:- run([eval(if(false, true, false))]).
+:- run([bind(x, bName), eval(x)]).
+:- run([bind(x, bMAbb(true)), eval(x), eval(if(x, false, x))]).
+:- run([eval((fn(x) -> x))]).
+:- run([eval((fn(x) -> x) $ (fn(x) -> x $ x))]).
+:- run([eval({[x = (fn(x) -> x), y = (fn(x) -> x) $ (fn(x) -> x)]})]).
+:- run([eval({[x = (fn(x) -> x), y = (fn(x) -> x) $ (fn(x) -> x)]} # x)]).
+:- run([eval("hello")]).
+:- run([eval(2.0 * 3.0 * (4.0 * 5.0))]).
+:- run([eval(0)]).
+:- run([eval(succ(pred(0)))]).
+:- run([eval(iszero(pred(succ(succ(0)))))]).
+:- run([eval((let(x) = true in x))]).
+:- run([eval({[1 = 0, 2 = 1.5]})]).
+:- halt.
+
